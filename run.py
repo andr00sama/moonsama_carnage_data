9ec5f35b79d9bd271643b016fbb774c079bfc9e2
andr00sama <122212181+andr00sama@users.noreply.github.com>
2023-01-09 17:34:17 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,3 @@
-from inputs import raw_result_array, raw_gganbu_array
-from functions import get_and_load_dictionary, sama_sorter
+from functions import attendance_counter
 
-for week in range (48,56):
-    week_index = week - 17
-    print("Week Number: " + str(week))
-    gganbu_dictionary = get_and_load_dictionary(raw_gganbu_array[week_index])
-    result_dictionary = get_and_load_dictionary(raw_result_array[week_index])
-    sama_sorter(result_dictionary, gganbu_dictionary)
-    print(" ")
+print(attendance_counter(17, 55))
