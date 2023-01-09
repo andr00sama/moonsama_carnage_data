b2a76e23e9cd67437490fa3eedbe839744cf6895
andr00sama <122212181+andr00sama@users.noreply.github.com>
2023-01-09 13:05:08 -0400

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+from inputs import raw_result_array, raw_gganbu_array
+from functions import get_and_load_dictionary, build_dictionary_array, sama_sorter
+
+for week in range (48,56):
+    week_index = week - 17
+    print("Week Number: " + str(week))
+    gganbu_dictionary = get_and_load_dictionary(raw_gganbu_array[week_index])
+    result_dictionary = get_and_load_dictionary(raw_result_array[week_index])
+    sama_sorter(result_dictionary, gganbu_dictionary)
+    print(" ")
